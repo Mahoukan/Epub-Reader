4a02174a34ab6bf53599d5882173f60eb6555989
Mahoukan <suriyakrishnans@gmail.com>
2025-07-09 21:32:55 +1200

    Some updates to the reader

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ fileInput.addEventListener("change", () => {
 
   const url = URL.createObjectURL(file);
   book = ePub(url);
+
   rendition = book.renderTo("viewer", {
     width: "100%",
     height: "100%",
   });
+
   rendition.display();
 
   book.loaded.navigation.then(nav => {
@@ -19,7 +21,9 @@ fileInput.addEventListener("change", () => {
     nav.toc.forEach(chapter => {
       const li = document.createElement("li");
       li.textContent = chapter.label;
-      li.addEventListener("click", () => rendition.display(chapter.href));
+      li.addEventListener("click", () => {
+        rendition.display(chapter.href);
+      });
       tocElement.appendChild(li);
     });
   });
